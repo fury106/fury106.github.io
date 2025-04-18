8b23ec43e8ce6f86f05e9525f7a362e569122d26
fury106 <indyverhoeven1@outlook.com>
2025-04-19 01:01:34 +0200

    test with queue time pages

--- wachttijden/walibi_be.js
+++ wachttijden/walibi_be.js
@@ -24,20 +24,21 @@ fetch(apiUrl)
             container.appendChild(rideElement);
         });
     })
-    .catch (error => {
-    console.error('Error fetching data:', error);
-    const container = document.getElementById('queue-times');
-    container.textContent = 'Er is een probleem opgetreden bij het ophalen van de wachttijden.';
-    });
-	
-fetch(apiUrl)
-    .then(response => {
-        console.log('API Response:', response);
-        if (!response.ok) {
-            throw new Error('Probleem bij het ophalen van de data');
-        }
-        return response.json();
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+    //.catch (error => {
+    //console.error('Error fetching data:', error);
+    //const container = document.getElementById('queue-times');
+    //container.textContent = 'Er is een probleem opgetreden bij het ophalen van de wachttijden.';
+    //});
+	;
+//fetch(apiUrl)
+//    .then(response => {
+//        console.log('API Response:', response);
+//        if (!response.ok) {
+//            throw new Error('Probleem bij het ophalen van de data');
+//        }
+//        return response.json();
+//    })
+//    .catch(error => {
+//        console.error('Error:', error);
+//    });
+//
